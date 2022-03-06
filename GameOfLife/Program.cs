fbe72e2560059ea2d98c8d589cc5bf807457654d
Daniel Garcia <danigpino@gmail.com>
2022-03-06 23:40:30 +0100

    Added commands to program

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -1,22 +1,84 @@
 ﻿using GameOfLife;
 
-var universe = new Universe();
-
-universe.Reset(new Cell(0, 0), new Cell(-1, 0), new Cell(1, 0), new Cell(0, 1), new Cell(0, -1));
-
 Console.Clear();
 Console.WriteLine("Game of life");
 
-var left = Console.CursorLeft;
-var top = Console.CursorTop;
+var running = true;
+var cells = new HashSet<Cell>();
 
-Console.WriteLine(universe.Print(-10, 10, -10, 10));
-
-while (true)
+while (running)
 {
-    Thread.Sleep(1000);
-    universe.Tick();
+    var commandArgs = ReadCommand();
+
+    if (commandArgs[0] == "exit")
+    {
+        Exit();
+    }
+
+    if (commandArgs[0] == "add")
+    {
+        Add(commandArgs);
+    }
+
+    if (commandArgs[0] == "start")
+    {
+        Start();
+    }
+}
+
+string[] ReadCommand()
+{
+    Console.Write(">");
+    var strings = Console.ReadLine()!.Split(' ');
+    return strings;
+}
+
+void Exit()
+{
+    running = false;
+}
+
+void Add(string[] commandArgs1)
+{
+    if (commandArgs1.Length != 3 || !long.TryParse(commandArgs1[1], out var x) ||
+        !long.TryParse(commandArgs1[2], out var y))
+    {
+        Console.WriteLine("usage: add <x_coordinate> <y_coordinate>");
+        return;
+    }
+
+    cells.Add(new Cell(x, y));
+    Console.WriteLine($"Live cell added at ({x},{y})");
+}
+
+void Start()
+{
+    var universe = new Universe();
+    universe.Reset(cells);
+
+    Console.Clear();
+    Console.WriteLine("Starting Game...");
+    var left = Console.CursorLeft;
+    var top = Console.CursorTop;
+
+    var gameRunning = true;
+
+    Task.Run(() =>
+    {
+        while (gameRunning)
+        {
+            var consoleKey = Console.ReadKey(true).Key;
+            gameRunning = consoleKey != ConsoleKey.Escape;
+        }
+    });
     
-    Console.SetCursorPosition(left, top);
-    Console.WriteLine(universe.Print(-10, 10, -10, 10));
+    while (gameRunning)
+    {
+        Console.SetCursorPosition(left, top);
+        Console.WriteLine(universe.Print(-10, 10, -10, 10));
+        Console.WriteLine("Press scape to finish execution");
+        
+        Thread.Sleep(1000);
+        universe.Tick();
+    }
 }
